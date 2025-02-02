a2a4aa2c479066672d4bc3fb5f549ed772fef09f
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2025-02-01 19:20:19 -0800

    Add first function

--- src/Program.fs
+++ src/Program.fs
@@ -1,2 +1,6 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// Define a new function to print a name.
+let printGreeting name =
+    printfn $"Hello {name} from F#!"
+
+// Call your new function!
+printGreeting "Ana"
